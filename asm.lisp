90f3d0525ebc4a26587ed17a688f542559b7def4
Paul Tarvydas <paultarvydas@gmail.com>
2019-03-13 14:00:44 -0400

    changed symbol emit to emit-p

--- asm.lisp
+++ asm.lisp
@@ -63,54 +63,54 @@
           while item do
           (assemble-1 item out t))))
 
-(defun assemble-1 (item outf emit) ;; emit = nil on pass 1, = t on pass 2
+(defun assemble-1 (item outf emit-p) ;; emit-p = nil on pass 1, = t on pass 2
   (cond
 
-   ((numberp item) (out-byte item outf emit)
+   ((numberp item) (out-byte item outf emit-p)
                    (incf *pc*))
 
-   ((characterp item) (out-byte (char-code item) outf emit)
+   ((characterp item) (out-byte (char-code item) outf emit-p)
                      (incf *pc*))
 
    ((listp item)
     (if (eq '= (first item))
         ;; equate
         (progn
-          (unless emit (assert (null (gethash (second item) *labels*))))
+          (unless emit-p (assert (null (gethash (second item) *labels*))))
           (setf (gethash (second item) *labels*) (third item)))
       (if (eq 'quote (first item))
           ;; char emit
           (progn (out-byte (char-code (char (format nil "~A" (second item)) 0))
-                           outf emit) (incf *pc*))
+                           outf emit-p) (incf *pc*))
 
         (let ((base (first item)) (name (second item)))
           (multiple-value-bind (b success) (gethash base *labels*)
-            (error-if (and emit (not success)) (format nil "ASM: base label ~A is not defined" base)
+            (error-if (and emit-p (not success)) (format nil "ASM: base label ~A is not defined" base)
                       :ex 'asm-error)
             (if (numberp name)
-                (if emit (let ((diff (+ b name)))
+                (if emit-p (let ((diff (+ b name)))
                            (setf *pc* (+ *pc* (calc-word-size diff)))
-                           (out-flexi-num diff outf emit))
+                           (out-flexi-num diff outf emit-p))
                   (setf *pc* (+ *pc* (calc-word-size 0))))
               
               (multiple-value-bind (v success) (gethash name *labels*)
-                (error-if (and emit (not success)) (format nil "ASM: label ~A is not defined in file" name) :ex 'asm-error)
-;(when (and emit (null v)) (break))
-                (if emit (let ((diff (- (if v v 0) b)))
+                (error-if (and emit-p (not success)) (format nil "ASM: label ~A is not defined in file" name) :ex 'asm-error)
+;(when (and emit-p (null v)) (break))
+                (if emit-p (let ((diff (- (if v v 0) b)))
                            (setf *pc* (+ *pc* (calc-word-size diff)))
-                           (out-flexi-num diff outf emit))
+                           (out-flexi-num diff outf emit-p))
                   (setf *pc* (+ *pc* (calc-word-size 0)))))))))))
     
    ((symbolp item)
     (case (symbol-first-char item)
 
-      (#\% (setf *pc* (+ (out-word (strip-to-int item emit) outf emit) *pc*)))
+      (#\% (setf *pc* (+ (out-word (strip-to-int item emit-p) outf emit-p) *pc*)))
 
-      (#\& (setf *pc* (+ (out-three (strip-to-int item emit) outf emit) *pc*)))
-      (#\^ (setf *pc* (+ (out-long (strip-to-int item emit) outf emit) *pc*)))
+      (#\& (setf *pc* (+ (out-three (strip-to-int item emit-p) outf emit-p) *pc*)))
+      (#\^ (setf *pc* (+ (out-long (strip-to-int item emit-p) outf emit-p) *pc*)))
 
       ; label definition - active only on first pass
-      (#\$ (when (not emit)
+      (#\$ (when (not emit-p)
              (let ((name (strip-to-symbol item)))
                (multiple-value-bind (v success) (gethash name *labels*) (declare (ignore v))
                  (error-if success (format nil "ASM: label ~A already defined" name)))
@@ -124,21 +124,21 @@
       ; label reference - all labels are pc-relative
       (#\? (let ((name (strip-to-symbol item)))
              (multiple-value-bind (v success) (gethash name *labels*)
-             (error-if (and emit (not success)) (format nil "ASM: label ~A is not defined" name) :ex 'asm-error)
+             (error-if (and emit-p (not success)) (format nil "ASM: label ~A is not defined" name) :ex 'asm-error)
              (if (prolog-p name)
                  (progn
                    (incf *pc* 3)
-                   (out-three v outf emit))
+                   (out-three v outf emit-p))
                (progn
                  (incf *pc* (calc-word-size v))
-                 (out-word v outf emit))))))
+                 (out-word v outf emit-p))))))
 
-      (otherwise (setf *pc* (+ (assemble-opcode item outf emit) *pc*)))))
+      (otherwise (setf *pc* (+ (assemble-opcode item outf emit-p) *pc*)))))
 
    ((stringp item)
     ;; use external call - get-constant - to install constant into table return an id
     (let ((id (fetch-constant item)))
-      (incf *pc* (out-word id outf emit))))
+      (incf *pc* (out-word id outf emit-p))))
 
    (t (assert nil))))
 
@@ -154,13 +154,13 @@
     (values (subseq n 0 p)
             (parse-integer (subseq n (1+ p))))))
 
-(defun assemble-opcode (item outf emit)
+(defun assemble-opcode (item outf emit-p)
   (multiple-value-bind (v succ) (gethash item *opcodes*)
     (error-if (not succ) (format nil "ASM: opcode ~A is not defined" item) :ex 'asm-error)
-    (out-byte v outf emit))
+    (out-byte v outf emit-p))
   1)
 
-(defun strip-to-int (item emit)
+(defun strip-to-int (item emit-p)
   "remove 1st character from symbol, then return it as an integer
    if second character is ? then resolve the label to an integer"
   (with-input-from-string (s (subseq (symbol-name item) 1))
@@ -168,7 +168,7 @@
       (when (symbolp r)
         (let ((name (strip-to-symbol r)) (temp-pc 0))
           (multiple-value-bind (v success) (gethash name *labels*)
-            (error-if (and emit (not success)) (format nil "ASM: label ~A is not defined" name) :ex 'asm-error)
+            (error-if (and emit-p (not success)) (format nil "ASM: label ~A is not defined" name) :ex 'asm-error)
             (setf temp-pc (+ *pc* (calc-word-size v)))
             (setf r (if v (- v temp-pc) 0)))))
       r)))
@@ -177,25 +177,25 @@
   "remove 1st character from symbol, then return it as a string"
   (intern (subseq (symbol-name item) 1)))
 
-(defun out-word (item outf emit)
-  (when emit 
-    (out-byte (logand #xff (ash item -8)) outf emit)
-    (out-byte (logand #xff item) outf emit))
+(defun out-word (item outf emit-p)
+  (when emit-p 
+    (out-byte (logand #xff (ash item -8)) outf emit-p)
+    (out-byte (logand #xff item) outf emit-p))
   2)
 
-(defun out-three (item outf emit)
-  (when emit 
-    (out-byte (logand #xff (ash item -16)) outf emit)
-    (out-byte (logand #xff (ash item -8)) outf emit)
-    (out-byte (logand #xff item) outf emit))
+(defun out-three (item outf emit-p)
+  (when emit-p 
+    (out-byte (logand #xff (ash item -16)) outf emit-p)
+    (out-byte (logand #xff (ash item -8)) outf emit-p)
+    (out-byte (logand #xff item) outf emit-p))
   3)
 
-(defun out-long (item outf emit)
-  (when emit 
-    (out-byte (logand #xff (ash item -24)) outf emit)
-    (out-byte (logand #xff (ash item -16)) outf emit)
-    (out-byte (logand #xff (ash item -8)) outf emit)
-    (out-byte (logand #xff item) outf emit))
+(defun out-long (item outf emit-p)
+  (when emit-p 
+    (out-byte (logand #xff (ash item -24)) outf emit-p)
+    (out-byte (logand #xff (ash item -16)) outf emit-p)
+    (out-byte (logand #xff (ash item -8)) outf emit-p)
+    (out-byte (logand #xff item) outf emit-p))
   4)
 
 (defun calc-word-size (item)
@@ -203,21 +203,21 @@
   (declare (ignore item))
   2)
 
-(defun out-flexi-num (item outf emit)
+(defun out-flexi-num (item outf emit-p)
   "write the flexinum, return final size; high-bit set means 1-byte, unset two bytes"
   (assert (zerop (logand #x80 (ash item -8))))
   (if nil ;(<= 0 item 127)
       (progn
-        (when emit (out-byte (logior #x80 item) outf emit))
+        (when emit-p (out-byte (logior #x80 item) outf emit-p))
         1)
     (progn
-      (when emit
-        (out-byte (logand #x7f (ash item -8)) outf emit)
-        (out-byte (logand #xff item) outf emit))
+      (when emit-p
+        (out-byte (logand #x7f (ash item -8)) outf emit-p)
+        (out-byte (logand #xff item) outf emit-p))
       2)))
 
-(defun out-byte (item outf emit)
-  (when emit
+(defun out-byte (item outf emit-p)
+  (when emit-p
     (put-byte outf (logand #xff item))))
 
 ;; temp stuff
